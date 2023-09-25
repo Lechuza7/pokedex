0e9a11166bcf85dcd40f16d241a5ecf33595c620
Marc Cuesta <marc.cuestacarabasa@gmail.com>
2023-09-25 19:32:39 +0200

    Fetch with Promise.all (single pokemon data added)

--- src/components/Pokemons.jsx
+++ src/components/Pokemons.jsx
@@ -1,15 +1,22 @@
 /* eslint-disable react/prop-types */
+import { useState } from 'react';
+import './Pokemons.css'
+
 function PokemonsList({ pokemons }) {
+  const [hover, setHover] = useState(true)
+  
   const renderPokemons = () => {
     return pokemons.map((pokemon) => (
-      <li className="pokemon" key={pokemon.name}>
-        <h3>{pokemon.name[0].toUpperCase() + pokemon.name.slice(1)}</h3>
+      <li className="poke-card" key={pokemon.name}>
         <img
-          src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${
-            pokemon.url.split("/")[6]
-          }.png`}
+          onMouseOver={() => setHover(false)}
+          onMouseOut={() => setHover(true)}
+          src={hover? pokemon.sprite1 : pokemon.sprite2}
           alt={pokemon.name}
         />
+        <h3>{pokemon.name[0].toUpperCase() + pokemon.name.slice(1)}</h3>
+        <h6>Id.: {pokemon.id}</h6>
+        <h6>Exp.: {pokemon.baseExperience}</h6>
       </li>
     ));
   };
--- src/services/pokemons.js
+++ src/services/pokemons.js
@@ -1,21 +1,34 @@
-
-export const searchPokemons = async({ search }) => {
-  
+export const searchPokemons = async ({ search }) => {
   try {
-    const response = await fetch("https://pokeapi.co/api/v2/pokemon?limit=10000&offset=0");
+    const response = await fetch(
+      "https://pokeapi.co/api/v2/pokemon?limit=10000&offset=0"
+    );
     const json = await response.json();
     const pokemons = json.results;
-    
-    const allPokemons = pokemons?.map((pokemon) => ({
-      name: pokemon.name,
-      url: pokemon.url
-    })) 
 
-    const searchedPokemons = allPokemons.filter((pokemon) => pokemon.name.includes(search))
-    
-    return searchedPokemons ? searchedPokemons : allPokemons
+    const allPokemons = await Promise.all(
+      pokemons.map(async (pokemon) => {
+        const pokemonResponse = await fetch(pokemon.url);
+        const pokemonData = await pokemonResponse.json();
+
+        return {
+          name: pokemonData.name,
+          url: pokemon.url,
+          sprite1: pokemonData.sprites.front_default,
+          sprite2: pokemonData.sprites.back_default,
+          baseExperience: pokemonData.base_experience,
+          id: pokemonData.id,
+          height: pokemonData.height
+        };
+      })
+    );
+
+    const searchedPokemons = allPokemons.filter((pokemon) =>
+      pokemon.name.includes(search)
+    );
+
+    return searchedPokemons.length > 0 ? searchedPokemons : allPokemons;
   } catch (e) {
-    throw new Error('Error buscando pokemons')
+    throw new Error("Error buscando pokemons");
   }
 };
-
